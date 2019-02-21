69ee4047054bfd9231938a897a1ada68480ced71
Nash Brancic <nashfft@gmail.com>
2019-02-21 02:14:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,16 +43,6 @@ def sum_array(array)
   array.inject(0){ |n,b| n + b } #(0) is a base case so it doesnt nil
 end
   
-# def add_s(array)
-#   array.collect do |word|
-#     if array[1] == word
-#       word
-#     else
-#       word + "s"
-#     end
-#   end
-# end
-
 def add_s(array)
   array.each_with_index.collect do |word|
     if array[1] == word
@@ -62,13 +52,3 @@ def add_s(array)
     end
   end
 end
-
-
-
-
-
-
-
-
-
-
